0abf8c02322419186663fb0376abe390f17037a1
Kevin Gustavo Garcia Aviles <kevin.garcia.aviles@udla.edu.ec>
2023-07-17 18:17:01 -0500

    Cambios detalle encuesta

--- SondeoBackend/Controllers/MedicionesController.cs
+++ SondeoBackend/Controllers/MedicionesController.cs
@@ -50,7 +50,9 @@ namespace SondeoBackend.Controllers
         [HttpGet]
         public async Task<ActionResult<Encuesta>> GetDetalleEncuesta(int id)
         {
-            return await _context.Encuestas.Include(e => e.CustomUser).Include(e => e.Local).Include(e => e.Medicion).Include(e => e.DetalleEncuestas).ThenInclude(e => e.Producto).FirstOrDefaultAsync(m => m.Id == id);
+            return await _context.Encuestas.Include(e => e.CustomUser).Include(e => e.Local).Include(e => e.Medicion).Include(e => e.DetalleEncuestas).ThenInclude(e => e.Producto).ThenInclude(e=> e.Propiedades)
+                .Include(e => e.DetalleEncuestas).ThenInclude(e => e.Producto).ThenInclude(e => e.Marca).Include(e => e.DetalleEncuestas).ThenInclude(e => e.Producto).ThenInclude(e => e.Categoria)
+                .FirstOrDefaultAsync(m => m.Id == id);
         }
 
         [Route("CrearMedicion")]
